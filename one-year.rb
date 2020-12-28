e97f2d92890575e5ba1411d24e59b869ff37a82d
Russell Haley <russ.haley@gmail.com>
2020-12-28 16:15:43 -0500

    Fix year generation.
    Thanks to @igneus for showing me how to loop through the calendar. Calendar now loops form the first liturgical day to the last and leap day works correctly.

--- one-year.rb
+++ one-year.rb
@@ -1,8 +1,19 @@
 require 'calendarium-romanum'
 require 'parse_psalms'
 
+year = Time.now.year
+if ARGV.length == 1 then
+	#TODO: check if it's a real year
+	year = ARGV[0].to_i
+end
 
-year = 2020
+puts "*********************************************************"
+puts "*	St. John Henry Newman Catholic Church		*"
+puts "*	Calendar For Litergical year: #{year} to #{year+1}	*"
+puts "*	Generated on #{Time.now}		*"
+puts "*********************************************************"
+puts""
+
 
 holy_days_file='data/psalms/Psalms-for-sundays-and-holy-days.txt'
 ordinary_days_file='data/psalms/Psalms-ordinary-weekdays.txt'
@@ -12,44 +23,19 @@ pfd = PsalmsForDays.new
 sanctorale = CalendariumRomanum::SanctoraleLoader.new.load_from_file 'data/ordinariate-en.txt'
 temporale = OrdinariateCalendar::Temporale.new(year)
 
-
-calendar = CalendariumRomanum::Calendar.new(year, sanctorale, temporale)
 psalms_ordinary = 	pfd.read_file(ordinary_days_file)
 psalms_holy_days = pfd.read_file(holy_days_file)
 psalms_saints = pfd.read_file(psalms_saints_file)
 
-start_date = Date.new(year , 11, 29)
-
-days = Array.new(365)
- 
-for i in 0..364
-        #puts start_date 
-        #THIS IS A HACK to deal with leap year. I don't know why it dies yet but it does so I just repeat the day
-        if start_date.month == 2 and start_date.day > 28 then
-          days[i] = calendar.day(start_date.month,start_date.day - 1)
-        else
-          days[i] = calendar.day(start_date.month,start_date.day)
-        end
-          # 	puts(start_date)
-	start_date += 1	
-end
-
-def generate_data(days, pfd, psalms_ordinary)
-	
-	for i in 0..364
-		day = days[i]
-		puts "Calendar Date: #{day.date.to_s} #{day.date.strftime('%A')}"
+#~ calendar = CalendariumRomanum::Calendar.new(year, sanctorale, temporale)
+#~ calendar.each do |day|
+CalendariumRomanum::Calendar.new(year, sanctorale, temporale).each do |day|
+	puts "Calendar Date: #{day.date.to_s} #{day.date.strftime('%A')}"
 		celebration = day.celebrations[0]
 		day.celebrations.each do | c |
 			puts "\t" + c.title
 		end		
 		psalms = pfd.get_ordinary_psalm(day.date, psalms_ordinary)
-	# 	puts celebration.symbol
-	# 	   psalms = pfd.get_saint_psalm(celebration.symbol.to_s, psalms_saints)
-	# 	   psalms = pfd.get_holy_day_psalm(celebration.symbol.to_s, psalms_holy_days)
 		puts "psalms: #{psalms.join(' : ')}"
 		puts "---------------------"
-	end
 end
-
-generate_data(days, pfd, psalms_ordinary)
